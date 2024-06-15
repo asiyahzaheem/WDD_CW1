e1524d5f932160b308ff75c141a430f473c44a06
asiyahzaheem <asiyahzaheem2006@gmail.com>
2024-06-15 20:08:55 +0530

    Initial styling and scripting for Splash Screen

---
+++ SplashScreen.js
@@ -0,0 +1,5 @@
+const splashScreenEl = document.getElementById('splashScreen');
+splashScreenEl.style.animation = 'fading 8s 1';
+setTimeout(() => {
+    splashScreenEl.style.display = 'none'
+}, 8000);
