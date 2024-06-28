72d83467840c11f06489b319e81a789d82913e88
asiyahzaheem <asiyahzaheem2006@gmail.com>
2024-06-28 08:08:35 +0530

    Changed design of Splash Screen

--- splash.js
+++ splash.js
@@ -1,5 +1,27 @@
-// const splashScreenEl = document.getElementById('splashScreen');
-// splashScreenEl.style.animation = 'fading 8s 1';
+const splashScreenEl = document.getElementById("splashScreen");
+
 // setTimeout(() => {
+//     console.log("gello")
+//     splashScreenEl.style.animation = 'fading 2s 1';
 //     splashScreenEl.style.display = 'none'
-// }, 8000);
+//     window.location.href = "/home.html"
+// }, 4000);
+
+/*
+window.onload = fadeout();
+
+function fadeout() { 
+    var intervalID = setInterval(function () { 
+        if (!fade.style.opacity) { 
+            fade.style.opacity = 1; 
+        } 
+        if (fade.style.opacity > 0) { 
+            fade.style.opacity -= 0.1; 
+        } 
+        else { 
+            clearInterval(intervalID); 
+        } 
+            }, 400); 
+        } 
+*/
+
