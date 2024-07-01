184c1d28a76546a7f736bae9d74bbce8a1aaa4d6
asiyahzaheem <asiyahzaheem2006@gmail.com>
2024-07-01 22:30:48 +0530

    Started product modal window

--- shop.js
+++ shop.js
@@ -2,5 +2,5 @@ const productEl = document.getElementById('product')
 const productModalEl = document.getElementById('product-modal')
 
 productEl.addEventListener("click", function() {
-    productModalEl.style.display = "block";
+    productModalEl.classList.toggle("show");
 })
