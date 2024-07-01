9b4d5da1f1885ee683ce5b1592445919dae8e743
asiyahzaheem <asiyahzaheem2006@gmail.com>
2024-07-01 13:42:18 +0530

    Added product modal window

---
+++ shop.js
@@ -0,0 +1,6 @@
+const productEl = document.getElementById('product')
+const productModalEl = document.getElementById('product-modal')
+
+productEl.addEventListener("click", function() {
+    productModalEl.style.display = "block";
+})
