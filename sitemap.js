4d4d94a867622f1c07998ab0c35a4579de193e93
Limesh-B <limesh075@gmail.com>
2024-07-17 16:00:19 +0530

    no changes

--- sitemap.js
+++ sitemap.js
@@ -5,7 +5,7 @@ const pages = [
     { id: 'groupId4', url: 'profile.html' },
     { id: 'groupId5', url: 'gallery.html' },
     { id: 'groupId6', url: 'shop.html' },
-    { id: 'groupId7', url: 'checkout.html' },
+    { id: 'groupId7', url: '_checkout.html' },
     { id: 'groupId8', url: 'team.html' },
     { id: 'groupId9', url: 'feedback.html' }
 ];
