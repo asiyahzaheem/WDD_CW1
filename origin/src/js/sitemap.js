c86fc8459208ee4747d3164f6f24122d69c5a604
Shan Fernando <shanemalka@outlook.com>
2024-07-21 22:56:06 +0530

    Modified origin folder

--- origin/src/js/sitemap.js
+++ origin/src/js/sitemap.js
@@ -1,22 +1,22 @@
 // Declare object array 
 const pages = [
-    { id: 'groupId1', url: '/asiyah/splash.html' },
-    { id: 'groupId2', url: '/venuja/profile.html' },
-    { id: 'groupId3', url: '/shan/home.html' },
-    { id: 'groupId4', url: '/venuja/profile.html' },
+    { id: 'groupId1', url: 'splash.html' },
+    { id: 'groupId2', url: 'profile.html' },
+    { id: 'groupId3', url: 'home.html' },
+    { id: 'groupId4', url: 'profile.html' },
     { id: 'groupId5', url: 'gallery.html' },
-    { id: 'groupId6', url: '/asiyah/shop.html' },
-    { id: 'groupId7', url: '/venuja/team.html' },
-    { id: 'groupId8', url: '/shan/feedback.html' },
+    { id: 'groupId6', url: 'shop.html' },
+    { id: 'groupId7', url: 'team.html' },
+    { id: 'groupId8', url: 'feedback.html' },
 
-    { id: 'groupId10', url: '/asiyah/contentpage_1.html' },
-    { id: 'groupId11', url: '/shan/contentpage_2.html' },
-    { id: 'groupId12', url: '/venuja/contentpage_3.html' },
+    { id: 'groupId10', url: 'contentPage_1.html' },
+    { id: 'groupId11', url: 'contentpage_2.html' },
+    { id: 'groupId12', url: 'contentpage_3.html' },
     { id: 'groupId13', url: 'contentpage_4.html' },
 
-    { id: 'groupId14', url: '/asiyah/editorpage_1.html' },
-    { id: 'groupId15', url: '/shan/editorpage_2.html' },
-    { id: 'groupId16', url: '/venuja/editorpage_3.html' },
+    { id: 'groupId14', url: 'pageEditor_1.html' },
+    { id: 'groupId15', url: 'editorpage_2.html' },
+    { id: 'groupId16', url: 'pageEditor.html' },
     { id: 'groupId17', url: 'editorpage_4.html' },
 
 ];
